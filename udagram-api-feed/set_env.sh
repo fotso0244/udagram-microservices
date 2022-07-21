0427f9228a85cb823264ee8cad7705db9a7816d2
aristide <fotso0244@gmail.com>
2022-07-21 11:26:33 -0700

    udagram microservices

---
+++ udagram-api-feed/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=aristide
+export POSTGRES_PASSWORD=Helene1964
+export POSTGRES_HOST=udagram-database.cr7oznbbrg4t.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::my-701067892882-udagram-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8080
