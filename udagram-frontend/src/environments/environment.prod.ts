99ae54d8edb42fb19fdfa771faa6cdf744f4abf8
aristide <fotso0244@gmail.com>
2022-07-24 03:44:07 +0100

    Updae project

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://acfecfae1a4954457b1cb48e8e4da394-754137164.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
